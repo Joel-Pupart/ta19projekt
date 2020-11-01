570710432d38d31387e2303992993be5a3fc6245
Joel-Pupart <56673710+Joel-Pupart@users.noreply.github.com>
2020-11-01 20:10:56 +0200

    Add files via upload

---
+++ routes.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'users' => [
+        'title' => 'User list',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . 'list.php'
+    ],
+    'users/add' => [
+        'title' => 'Add user',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . 'add.php'
+    ],
+    'users/edit' => [
+        'title' => 'Edit list',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'users' . DIRECTORY_SEPARATOR . 'edit.php'
+    ],
+    '404' => [
+        'title' => '404',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . '404.php'
+    ],
+    'blog' => [
+        'title' => 'Blog',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'blog' . DIRECTORY_SEPARATOR . 'list.php'
+    ],
+    'blog/post' => [
+        'title' => '',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'blog' . DIRECTORY_SEPARATOR . 'post.php'
+    ],
+    'blog/add' => [
+        'title' => 'New blogpost',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'blog' . DIRECTORY_SEPARATOR . 'add.php'
+    ],
+    'blog/post/edit' => [
+        'title' => 'Edit blogpost',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'blog' . DIRECTORY_SEPARATOR . 'edit.php'
+    ],
+    'login' => [
+        'title' => 'Login',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'login.php'
+    ],
+    'logout' => [
+        'title' => '',
+        'file_location' => __DIR__ . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'logout.php'
+    ],
+];
